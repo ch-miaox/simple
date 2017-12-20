01332237ec9122d2308df564d8c209f8945d511b
Liu Wen <liuwen@szprism.com>
2017-12-20 03:27:47 +0000

    user e i d

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,12 @@ use App\Http\Requests;
 use Auth;
 class SessionsController extends Controller
 {
+  public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     //
     public function create()
     {
